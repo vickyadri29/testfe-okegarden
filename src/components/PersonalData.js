8b268c7c11081f709ba3291377c001d778aa3acb
vickyadri29 <vickyadri290501@gmail.com>
2022-09-05 11:01:23 +0800

    feat/datainformation

--- src/components/PersonalData.js
+++ src/components/PersonalData.js
@@ -1,6 +1,10 @@
+import { DataInformation } from "../data";
+
 const PersonalData = () => {
+  const { gardenLand, gardenTheme, estimate } = DataInformation;
+
   return (
-    <section className="m-8 bg-white rounded-md drop-shadow-md">
+    <section className="m-8 bg-white rounded-md drop-shadow-md text-gray-700">
       {/* Personal Data Information */}
       <div className="max-w-7xl m-auto p-20 ">
         <div className="m-5">
@@ -50,57 +54,71 @@ const PersonalData = () => {
       {/* Personal Data Information */}
       <div className="max-w-7xl m-auto p-20">
         <div className="m-5">
-          <h2>Informasi Konsultasi Taman</h2>
-          <div>
-            <div>
-              <h3>Lahan Taman</h3>
+          <h2 className="font-bold text-3xl">Informasi Konsultasi Taman</h2>
+          <div className="">
+            <div className="flex flex-col mt-5 mb-2 gap-1">
+              <h3 className="font-bold text-[17px]">Lahan Taman</h3>
               <p>Pilih jumlah lahan taman yang akan dibuat</p>
             </div>
-            <div>
-              <div>
-                <p>1 Lahan Taman</p>
-              </div>
-              <div>
-                <p>2 Lahan Taman</p>
-              </div>
-              <div>
-                <p>Lebih dari 2 Lahan Taman</p>
-              </div>
+            <div className="flex gap-5 mt-2 my-8">
+              {gardenLand.map((data, index) => {
+                const { land } = data;
+                return (
+                  <div className="border-2 px-5 py-3 rounded-lg border-green-800">
+                    <p className="text-green-800 font-bold">{land}</p>
+                  </div>
+                );
+              })}
             </div>
           </div>
           <div>
-            <div>
-              <h3>Tema Taman</h3>
+            <div className="flex flex-col mt-5 mb-2 gap-1">
+              <h3 className="font-bold text-[17px]">Tema Taman</h3>
               <p>Pilih tema taman yang akan dibuat</p>
             </div>
             <div>
-              <p><span>Catatan : </span>Jika anda belum memiliki inspirasi tema taman maka anda bisa melewati tahapan ini, konsultan kami akan memberikan rekomendasi terbaik untuk taman anda.</p>
+              <p className="bg-[#FFF2D1] p-5 font-bold rounded-lg my-5 w-[710px]">
+                <span className="font-extrabold">Catatan : </span>Jika anda
+                belum memiliki inspirasi tema taman maka anda bisa melewati
+                tahapan ini, konsultan kami akan memberikan rekomendasi terbaik
+                untuk taman anda.
+              </p>
             </div>
           </div>
-          <div>
-            <div>
-              <img src="" alt="" />
-              <div>
-                <h3>Taman Minimalis</h3>
-                <p>Taman yang indah dilahan yang sempit</p>
-              </div>
-              <p>Pilih</p>
-            </div>
-            <div>
-              <img src="" alt="" />
-              <div>
-                <h3>Taman Kering</h3>
-                <p>Taman yang dirancang untuk menghasilkan suasana alam didalam dan diluar rumah</p>
-              </div>
-              <p>Pilih</p>
-            </div>
-            <div>
-              <img src="" alt="" />
-              <div>
-                <h3>Taman Tropis</h3>
-                <p>Taman yang paling sesuai dengan iklim di Indonesia</p>
-              </div>
-              <p>Pilih</p>
+          <div className="flex gap-5">
+            {gardenTheme.map((data, index) => {
+              const { img, title, desc, choose } = data;
+              return (
+                <div
+                  key={index}
+                  className="flex flex-col border gap-2 rounded-lg"
+                >
+                  <img src={img} alt="" className="w-full rounded-lg" />
+                  <div className="p-5 flex flex-col gap-10">
+                    <div className="">
+                      <h3 className="font-bold text-[17px]">Taman Kering</h3>
+                      <p>
+                        Taman yang dirancang untuk menghasilkan suasana alam
+                        didalam dan diluar rumah
+                      </p>
+                    </div>
+                    <p className="flex justify-end text-green-800 underline underline-offset-4 cursor-pointer">Pilih</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="my-5">
+            <h3 className="font-bold text-[17px] my-2">Estimasi Ukuran Taman</h3>
+            <div className="flex gap-5">
+              {estimate.map((data, index) => {
+                const { size } = data;
+                return (
+                  <div key={index} className="border-2 border-green-700 px-3 py-2 rounded-lg">
+                    <p className="text-green-700 font-bold">{size}</p>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
