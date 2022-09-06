e5716a98ab3e34a99afc05f37c0118c7c61a611b
vickyadri29 <vickyadri290501@gmail.com>
2022-09-06 09:28:02 +0800

    update readme.md

--- src/components/PersonalData.js
+++ src/components/PersonalData.js
@@ -11,7 +11,7 @@ const PersonalData = () => {
         {/* Garden Consult Information */}
         <GardenConsulting />
         {/* Button */}
-        <div className="flex justify-end md:p-10 p-3 mx-14 gap-5">
+        <div className="flex md:justify-end justify-center md:p-10 p-3 mx-14 gap-5">
           <button className="md:px-4 px-4 py-2 bg-gray-100 text-green-700 font-semibold md:text-base text-xs rounded-lg transition-all duration-200 hover:bg-green-700 hover:text-white active:bg-green-700 focus:outline-none focus:ring focus:ring-green-300">Batal</button>
           <button className="md:px-4 px-4 py-2 bg-green-800 text-white font-semibold md:text-base text-xs rounded-lg active:bg-green-700 focus:outline-none focus:ring focus:ring-green-300">OKE</button>
         </div>
